fbfa7e8ac61088a055176aa4a636dbe8cd742b53
Xeltol <64264529+Xeltol@users.noreply.github.com>
2020-06-16 18:30:06 +0700

    Create lastIndex.js

---
+++ lastIndex.js
@@ -0,0 +1,9 @@
+/*
+  last index in array
+*/
+
+let nums = [1, 1, 1, 2, 2, 3, 4, 4, 4, 5, 1, 6, 6, 1, 5, 6, 6, 6, 7, 7, 8, 8, 8, 8, 8, 9, 9, 9, 7];
+
+let lastIndex = nums.lastIndexOf(1);
+
+console.log(lastIndex)
