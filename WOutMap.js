29a491067cd516b7c3eb1f1e966003e3dddabd72
Xeltol <64264529+Xeltol@users.noreply.github.com>
2020-06-16 18:19:07 +0700

    Create WOutMap.js

---
+++ WOutMap.js
@@ -0,0 +1,12 @@
+/*
+  Перебор без использования map()
+*/
+
+let new_arr = [
+    { type: int, length: 7 },
+    { type: char, length: 3 },
+    { type: Byte, length: 2 },
+];
+
+let typeName = Array.from(typeName, ({type}) => type);
+console.log(typeName)
