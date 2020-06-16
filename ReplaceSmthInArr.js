398ad1a78ffdd5dd52f392b2c08658220e6fd04a
Xeltol <64264529+Xeltol@users.noreply.github.com>
2020-06-16 18:12:22 +0700

    Create ReplaceSmthInArr.js

---
+++ ReplaceSmthInArr.js
@@ -0,0 +1,10 @@
+/*
+  Replace something in array
+*/
+
+let arr = ["One", "Two", "Three", "Four", "Five"];
+
+// 1st: place in arr; 2nd: amount of things we want to change
+arr.splice(0, 2, "Six", "Seven");
+
+console.log(arr)
