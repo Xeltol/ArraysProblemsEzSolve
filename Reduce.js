1d55f41439c37faa17458d5f298cec94651b1f2e
Xeltol <64264529+Xeltol@users.noreply.github.com>
2020-06-16 18:14:07 +0700

    Update Reduce.js

--- Reduce.js
+++ Reduce.js
@@ -1,7 +1,5 @@
 /* 
-  You have to sum all numbers in array
-  
-  Нужно сложить все числа в массиве
+  Sum all numbers in array
 */
 
 let nums = [1, 1, 1, 2, 2, 3, 4, 4, 4, 5, 6, 6, 6, 6, 6, 7, 7, 8, 8, 8, 8, 8, 9, 9, 9]
